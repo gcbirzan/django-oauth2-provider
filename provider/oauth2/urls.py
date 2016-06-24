f5d15933cc6be8c4fbe0ff6fe5a050e56b22a82f
George-Cristian Bîrzan <g.birzan@mdg.bg>
2016-06-24 15:31:14 +0300

    Django 1.10 support.

--- provider/oauth2/urls.py
+++ provider/oauth2/urls.py
@@ -39,7 +39,7 @@ from ..compat.urls import *
 from .views import Authorize, Redirect, Capture, AccessTokenView
 
 
-urlpatterns = patterns('',
+urlpatterns = (
     url('^authorize/?$',
         login_required(Capture.as_view()),
         name='capture'),
